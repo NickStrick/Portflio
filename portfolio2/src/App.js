f3c0c5b743552655418bbff4009a9d5a76d21baa
Nick Stricker <strickerdev@gmail.com>
2024-05-31 19:51:39 -0500

    App creation and Stylization

---
+++ portfolio2/src/App.js
@@ -0,0 +1,73 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+import React, { useState } from 'react';
+
+import DotUnderline from './images/DottedLine.js';
+import NsLogo from './images/NS-trans-1.png'
+
+// Home component
+const Home = () => (
+  <div className="content-container">
+    <h2>Home</h2>
+    <p>Welcome to the Home page!</p>
+  </div>
+);
+
+// About component
+const About = () => (
+  <div className="content-container">
+    <h2>About</h2>
+    <p>This is the About page.</p>
+  </div>
+);
+
+function App() {
+  const [path, setPath] = useState(window.location.pathname);
+  console.log(window.location.pathname)
+  let appContainer = 'app-container'
+  if(path == '/experience') appContainer += ' experience-container'
+  if(path == '/projects') appContainer += ' projects-container'
+
+  return (
+    <Router>
+      <div className={appContainer}>
+        <nav className="navbar">
+          
+          <div className="container"><img className="logo-img" src={NsLogo} />
+            <ul>
+              <li className="nav-li">
+                <Link onClick={()=> setPath('/')} to="/" className={path == '/'?'nav-link active':'nav-link'} >Home<span>Home</span></Link>
+                <div className={path == '/'?'nav-underline underline active':'nav-underline underline'} ><DotUnderline fillColor={'#98FF98'} /></div>
+              </li>
+              
+              <li className="nav-li">
+                <Link onClick={()=> setPath('/projects')} to="/projects" className={path == '/projects'?'nav-link active':'nav-link'} >Projects<span>Projects</span></Link>
+                <div className={path == '/projects'?'nav-underline underline active':'nav-underline underline'} ><DotUnderline fillColor={'#98FF98'} /></div>
+              </li>
+              <li className="nav-li">
+                <Link onClick={()=> setPath('/skills')} to="/skills" className={path == '/skills'?'nav-link active':'nav-link'} >Skills<span>Skills</span></Link>
+                <div className={path == '/skills'?'nav-underline underline active':'nav-underline underline'} ><DotUnderline fillColor={'#98FF98'} /></div>
+              </li>
+              <li className="nav-li">
+                <Link onClick={()=> setPath('/experience')} to="/experience" className={path == '/experience'?'nav-link active':'nav-link'} >Experience<span>Experience</span></Link>
+                <div className={path == '/experience'?'nav-underline underline active':'nav-underline underline'} ><DotUnderline fillColor={'#98FF98'} /></div>
+              </li>
+              <li className="nav-li">
+                <Link onClick={()=> setPath('/about')} to="/about" className={path == '/about'?'nav-link active':'nav-link'} >About<span>About</span></Link>
+                <div className={path == '/about'?'nav-underline underline active':'nav-underline underline'} ><DotUnderline fillColor={'#98FF98'} /></div>
+              </li>
+            </ul>
+            <button className="nav-contact">Contact Me</button>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ portfolio2/src/images/DottedLine.js
@@ -0,0 +1,11 @@
+export default function Logo({ fillColor }) {
+    return (
+        <svg height="5" viewBox="0 0 200 5" width="200" fill={fillColor} stroke={fillColor}>
+  <line x1="2.44336" x2="3.44336" y1="1.5" y2="1.5" strokeWidth={3} />
+  <line x1="8.44336" x2="11.4434" y1="1.5" y2="1.5" strokeWidth={3}/>
+  <line x1="17.4434" x2="24.4434" y1="1.5" y2="1.5" strokeWidth={3}/>
+  <line x1="30.4434" x2="45.4434" y1="1.5" y2="1.5" strokeWidth={3}/>
+  <line x1="51.4434" x2="1000" y1="1.5" y2="1.5" strokeWidth={3} />
+</svg>
+    );
+  }
