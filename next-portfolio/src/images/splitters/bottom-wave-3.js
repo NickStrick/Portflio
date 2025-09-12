2083eda1368b35cf4058f898241f7f154c32f024
Nick Stricker <strickerdev@gmail.com>
2025-09-12 17:25:53 -0500

    Add images

---
+++ next-portfolio/src/images/splitters/bottom-wave-3.js
@@ -0,0 +1,38 @@
+
+export default function Logo({ fillColor='#28da00' }) {
+    return (
+        
+        <svg className="split-svg split-svg-wave-2 footer-wave"  xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 1024 562">
+        <g clip-path="url(#clip0_9174_234436)">
+        <g filter="url(#filter0_d_9174_234436)">
+        <path fill="url(#paint0_linear_9174_234436)" d="M536.91 258.845c-188.148 265.589-427.483-244.818-726.4-23.82-211.018 156.012-32.025 260.972 91.49 309.21h1163.98l276.54-381.444s-46.78-277.134-243.48-67.5C902.336 304.927 725.058-6.743 536.91 258.846Z"/>
+        </g>
+        <path fill="url(#paint1_linear_9174_234436)" d="M512.334 299.85C291.226 540.225 221.663 160.056-154.553 416.352c-76.26 51.952-113.95 89.709-125.572 129.647H1229.98l274.68-341.638s-303.11-347.643-454.33-93.784C899.098 364.435 733.442 59.475 512.334 299.85Z"/>
+        <path fill="#fff" d="M1525.94 282.91c-66.45 124.52 235.05 390.53 23.17 264.103-211.89-126.428-513.94 427.508-839.104 148.32C384.84 416.145 207.883 908.052-37.766 724.501c-245.649-183.552-94.339 253.138-217.896 68.03-123.556-185.108 71.554-157.642 239.772-353.32s244.936 117.563 500.225-95.784c255.289-213.346 462.491 27.719 671.505-244.223 209.02-271.941 614.12-273.54 370.1 183.706Z"/>
+        </g>
+        <defs>
+        <linearGradient id="paint0_linear_9174_234436" x1="477.172" x2="347.405" y1="357.335" y2="-15.236" gradientUnits="userSpaceOnUse">
+        <stop stop-color={fillColor}/>
+        <stop offset="1" stop-color="#28da00"/>
+        </linearGradient>
+        <linearGradient id="paint1_linear_9174_234436" x1="894.45" x2="364.156" y1="146.14" y2="344.71" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#28da00"/>
+        <stop offset="1" stop-color="#28da00"/>
+        </linearGradient>
+        <clipPath id="clip0_9174_234436">
+        <path fill="#fff" d="M-128 0h1280v561.333H-128z"/>
+        </clipPath>
+        <filter id="filter0_d_9174_234436" width="1664.33" height="562.145" x="-297.107" y="3.981" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feColorMatrix in="SourceAlpha" result="hardAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+        <feOffset dx="7.297" dy="4.49"/>
+        <feGaussianBlur stdDeviation="8.7"/>
+        <feComposite in2="hardAlpha" operator="out"/>
+        <feColorMatrix values="0 0 0 0 0.379167 0 0 0 0 0.254547 0 0 0 0 0.0126389 0 0 0 0.1 0"/>
+        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_9174_234436"/>
+        <feBlend in="SourceGraphic" in2="effect1_dropShadow_9174_234436" result="shape"/>
+        </filter>
+        </defs>
+        </svg>
+    );
+  }
---
+++ next-portfolio/src/images/svg/projCard.js
@@ -0,0 +1,13 @@
+export default function Split({ fillColor, index }) {
+    return (
+        <svg class="proj-svg" id={"proj-svg-"+index} viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" version="1.1">
+                    <defs> 
+                        <linearGradient id={"proj-gradient-"+index} x1="0" x2="1" y1="1" y2="0">
+                            <stop id="stop1" stop-color="rgb(35,40,40)" offset="0%"></stop>
+                            <stop id="stop2" stop-color="rgb(35,40,40)" offset="100%"></stop>
+                        </linearGradient>
+                    </defs>
+                <path stroke="transparent"  d="M21.7,-35.5C28.2,-33.8,33.5,-28.1,35.5,-21.5C37.5,-15,36,-7.5,35.3,-0.4C34.5,6.6,34.4,13.2,32.1,19.2C29.8,25.2,25.2,30.5,19.5,33.2C13.8,35.9,6.9,35.9,0.6,34.9C-5.6,33.8,-11.3,31.6,-17.2,29C-23.1,26.4,-29.3,23.5,-32.9,18.6C-36.5,13.7,-37.5,6.8,-37,0.3C-36.5,-6.3,-34.5,-12.5,-31.7,-18.8C-28.9,-25,-25.2,-31.1,-19.8,-33.4C-14.3,-35.6,-7.2,-34,0.2,-34.4C7.6,-34.8,15.2,-37.1,21.7,-35.5Z" width="100%" height="100%" transform="translate(50 50)" stroke-width="0" ></path>
+              </svg>
+    );
+  }
